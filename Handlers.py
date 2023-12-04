26775210f4e5172b1281e594302aa151cdb7d262
Artem Voytenko <av.voytenko@gmail.com>
2023-12-04 23:10:09 +0300

    hm

--- Handlers.py
+++ Handlers.py
@@ -5,10 +5,8 @@ from time import time
 import logging
 import os
 
-import matplotlib.pyplot as plt  # библиотека визуализации
 import numpy as np
 import pandas as pd
-import seaborn as sns
 from scipy import stats  # библиотека для расчетов
 from scipy.stats import norm
 from scipy.stats import t
@@ -1641,3 +1639,63 @@ def alert(sec=3):
 
     # Produce the beep sound
     winsound.Beep(frequency, duration)
+
+
+def reduce_memory_usage(df, verbose=True):
+    """
+    Reduce the memory usage of a pandas DataFrame by downcasting numeric columns
+    to more memory-efficient data types.
+
+    Parameters:
+    df (pd.DataFrame): The DataFrame whose memory usage is to be optimized.
+    verbose (bool): If True, prints the memory reduction information.
+
+    Returns:
+    pd.DataFrame: A DataFrame with optimized memory usage.
+    """
+
+    # Calculate the initial memory usage of the DataFrame
+    start_mem = df.memory_usage().sum() / 1024**2
+
+    # Iterate over each column that is of a numeric data type
+    for col in df.select_dtypes(include=["number"]).columns:
+        # Get the data type of the current column
+        col_type = df[col].dtype
+
+        # Check if the column is of integer type
+        if pd.api.types.is_integer_dtype(col_type):
+            # Calculate the minimum and maximum values in the column
+            c_min, c_max = df[col].min(), df[col].max()
+            # Downcast the column to the smallest possible integer type
+            # that can accommodate all the values in the column
+            if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                df[col] = df[col].astype(np.int8)
+            elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                df[col] = df[col].astype(np.int16)
+            elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                df[col] = df[col].astype(np.int32)
+            else:
+                df[col] = df[col].astype(np.int64)
+        # If the column is not an integer type, it is a float
+        else:
+            # Similarly, downcast the column to the smallest possible
+            # floating point type
+            c_min, c_max = df[col].min(), df[col].max()
+            if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
+                df[col] = df[col].astype(np.float16)
+            elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                df[col] = df[col].astype(np.float32)
+            else:
+                df[col] = df[col].astype(np.float64)
+
+    # Calculate the final memory usage after optimization
+    end_mem = df.memory_usage().sum() / 1024**2
+
+    # If verbose is True, print the amount of memory saved
+    if verbose:
+        print(
+            f"Mem. usage decreased to {end_mem:.2f} Mb ({100 * (start_mem - end_mem) / start_mem:.1f}% reduction)"
+        )
+
+    # Return the DataFrame with reduced memory usage
+    return df
